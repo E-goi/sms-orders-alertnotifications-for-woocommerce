27486479e438bbfa81e182b31f5d94bcb2b592d9
fbobiano <fbobiano@e-goi.com>
2021-09-16 10:23:17 +0100

    chore: code format

--- smart-marketing-addon-sms-order.php
+++ smart-marketing-addon-sms-order.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The plugin bootstrap file
  *
@@ -32,45 +31,58 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-$recipents = json_decode(get_option('egoi_sms_order_recipients'));
-if (isset($recipents->egoi_reminders) && $recipents->egoi_reminders == 1 && !defined('ALTERNATE_WP_CRON')) {
-    define('ALTERNATE_WP_CRON', true);
+$recipents = json_decode( get_option( 'egoi_sms_order_recipients' ) );
+if ( isset( $recipents->egoi_reminders ) && 1 === $recipents->egoi_reminders && ! defined( 'ALTERNATE_WP_CRON' ) ) {
+	define( 'ALTERNATE_WP_CRON', true );
 }
 
 add_action( 'admin_init', 'smsonw_child_plugin_has_parent_plugin' );
+
+/**
+ * Check if main plugin is activated
+ */
 function smsonw_child_plugin_has_parent_plugin() {
-    $parant_plugin = plugin_dir_path( __DIR__ ).'smart-marketing-for-wp';
+	$parant_plugin = plugin_dir_path( __DIR__ ) . 'smart-marketing-for-wp';
 
-    if (!is_dir($parant_plugin)) {
+	if ( ! is_dir( $parant_plugin ) ) {
 
-        add_action( 'admin_notices', 'smsonw_parent_plugin_notice' );
+		add_action( 'admin_notices', 'smsonw_parent_plugin_notice' );
 
-        deactivate_plugins( plugin_basename( __FILE__ ) );
+		deactivate_plugins( plugin_basename( __FILE__ ) );
 
-    } else if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !is_plugin_active( 'smart-marketing-for-wp/egoi-for-wp.php' ) ) {
-        add_action( 'admin_notices', 'smsonw_child_plugin_notice' );
+	} elseif ( is_admin() && current_user_can( 'activate_plugins' ) && ! is_plugin_active( 'smart-marketing-for-wp/egoi-for-wp.php' ) ) {
+		add_action( 'admin_notices', 'smsonw_child_plugin_notice' );
 
-        deactivate_plugins( plugin_basename( __FILE__ ) );
+		deactivate_plugins( plugin_basename( __FILE__ ) );
 
-        if ( isset( $_GET['activate'] ) ) {
-            unset( $_GET['activate'] );
-        }
-    }
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+	}
 }
 
-function smsonw_parent_plugin_notice(){
-    ?><div class="notice notice-error is-dismissible">
-    <p>
-        <?php _e('To use this plugin, you first need to install', 'smart-marketing-addon-sms-order');?>
-        <a href="https://wordpress.org/plugins/smart-marketing-for-wp/" target="_blank">Smart Marketing SMS and Newsletters Forms by E-goi</a>
-    </p>
-    </div><?php
+/**
+ * Plugin notice error to install main plugin
+ */
+function smsonw_parent_plugin_notice() {
+	?><div class="notice notice-error is-dismissible">
+	<p>
+		<?php _e( 'To use this plugin, you first need to install', 'smart-marketing-addon-sms-order' ); ?>
+		<a href="https://wordpress.org/plugins/smart-marketing-for-wp/" target="_blank">Smart Marketing SMS and Newsletters Forms by E-goi</a>
+	</p>
+	</div>
+	<?php
 }
 
-function smsonw_child_plugin_notice(){
-    ?><div class="notice notice-error is-dismissible">
-    <p><?php _e('By removing this plugin, you will no longer be able to use the SMS plugin', 'smart-marketing-addon-sms-order');?></p>
-    </div><?php
+/**
+ * Plugin notice when removing
+ */
+function smsonw_child_plugin_notice() {
+	?>
+	<div class="notice notice-error is-dismissible">
+	<p><?php _e( 'By removing this plugin, you will no longer be able to use the SMS plugin', 'smart-marketing-addon-sms-order' ); ?></p>
+	</div>
+	<?php
 }
 
 /**
@@ -123,50 +135,57 @@ function run_smart_marketing_addon_sms_order() {
 
 }
 
-function run_smart_marketing_addon_sms_order_action($action){
-    $plugin = new Smart_Marketing_Addon_Sms_Order_Public();
-    $plugin->$action();
+/**
+ * Begins plugin action.
+ *
+ * @param function $action action function.
+ */
+function run_smart_marketing_addon_sms_order_action( $action ) {
+	$plugin = new Smart_Marketing_Addon_Sms_Order_Public();
+	$plugin->$action();
 }
 
 /**
- * @param false $url
+ * Add multiple produtcts to cart.
+ *
+ * @param false $url url.
  *
  * @return bool
  */
 function egoi_add_multiple_products_to_cart( $url = false ) {
 
-    if ( ! class_exists( 'WC_Form_Handler' ) || empty( $_REQUEST['create-cart'] ) || false === strpos( $_REQUEST['create-cart'], ',' ) ) {
-        return false;
-    }
-    add_filter( 'wc_add_to_cart_message_html', '__return_false' );
-    $product_ids = explode( ',', $_REQUEST['create-cart'] );
+	if ( ! class_exists( 'WC_Form_Handler' ) || empty( $_REQUEST['create-cart'] ) || false === strpos( $_REQUEST['create-cart'], ',' ) ) {
+		return false;
+	}
+	add_filter( 'wc_add_to_cart_message_html', '__return_false' );
+	$product_ids = explode( ',', sanitize_text_field( wp_unslash( $_REQUEST['create-cart'] ) ) );
 
-	if(!empty($_REQUEST['sid_eg'])){
+	if ( ! empty( $_REQUEST['sid_eg'] ) ) {
 		global $wpdb;
-		$_SESSION['sid_eg'] = filter_var($_REQUEST['sid_eg'], FILTER_SANITIZE_STRING);
-		$wpdb->update($wpdb->prefix.'egoi_sms_abandoned_carts', ['status' => 'clicked'], ['php_session_key' => $_SESSION['sid_eg']]);
+		$_SESSION['sid_eg'] = filter_var( $_REQUEST['sid_eg'], FILTER_SANITIZE_STRING );
+		$wpdb->update( $wpdb->prefix . 'egoi_sms_abandoned_carts', array( 'status' => 'clicked' ), array( 'php_session_key' => $_SESSION['sid_eg'] ) );
 	} else {
-	    return false;
-    }
+		return false;
+	}
 
 	WC()->cart->empty_cart();
-    foreach ( $product_ids as $id_and_quantity ) {
+	foreach ( $product_ids as $id_and_quantity ) {
 
-	    $id_and_quantity = explode( ':', $id_and_quantity );
+		$id_and_quantity = explode( ':', $id_and_quantity );
 
-	    $product_id = $id_and_quantity[0];
-	    $quantity = ! empty( $id_and_quantity[1] ) ? absint( $id_and_quantity[1] ) : 1;
+		$product_id = $id_and_quantity[0];
+		$quantity   = ! empty( $id_and_quantity[1] ) ? absint( $id_and_quantity[1] ) : 1;
 
-	    $adding_to_cart    = wc_get_product( $product_id );
-        if ( ! $adding_to_cart ) {
-            continue;
-        }
+		$adding_to_cart = wc_get_product( $product_id );
+		if ( ! $adding_to_cart ) {
+			continue;
+		}
 
-	    WC()->cart->add_to_cart( $product_id, $quantity );
+		WC()->cart->add_to_cart( $product_id, $quantity );
 
-    }
+	}
 
-    return true;
+	return true;
 }
 
 add_action( 'wp_loaded', 'egoi_add_multiple_products_to_cart', 15 );
@@ -175,64 +194,74 @@ add_action( 'wp_loaded', 'egoi_add_multiple_products_to_cart', 15 );
 /**
  * Invoke class private method
  *
- * @param string $class_name
- * @param string $methodName
+ * @param string $class_name class name.
+ * @param string $method_name method name.
  *
  * @return  mixed
- * @throws ReflectionException
- *
+ * @throws ReflectionException Reflection exception.
  */
-function woo_hack_invoke_private_method( $class_name, $methodName ) {
-    if ( version_compare( phpversion(), '5.3', '<' ) ) {
-        throw new Exception( 'PHP version does not support ReflectionClass::setAccessible()', __LINE__ );
-    }
+function woo_hack_invoke_private_method( $class_name, $method_name ) {
+	if ( version_compare( phpversion(), '5.3', '<' ) ) {
+		throw new Exception( 'PHP version does not support ReflectionClass::setAccessible()', __LINE__ );
+	}
 
-    $args = func_get_args();
-    unset( $args[0], $args[1] );
-    $reflection = new ReflectionClass( $class_name );
-    $method = $reflection->getMethod( $methodName );
-    $method->setAccessible( true );
+	$args = func_get_args();
+	unset( $args[0], $args[1] );
+	$reflection = new ReflectionClass( $class_name );
+	$method     = $reflection->getMethod( $$method_name );
+	$method->setAccessible( true );
 
-    //$args = array_merge( array( $class_name ), $args );
-    $args = array_merge( array( $reflection ), $args );
-    return call_user_func_array( array( $method, 'invoke' ), $args );
+	$args = array_merge( array( $reflection ), $args );
+	return call_user_func_array( array( $method, 'invoke' ), $args );
 }
 
-add_action('wp_ajax_process_cellphone', 'process_cellphone');
-add_action('wp_ajax_nopriv_process_cellphone', 'process_cellphone');
-function process_cellphone(){
-    run_smart_marketing_addon_sms_order_action(__FUNCTION__);
+add_action( 'wp_ajax_process_cellphone', 'process_cellphone' );
+add_action( 'wp_ajax_nopriv_process_cellphone', 'process_cellphone' );
+
+/**
+ * Process cellphone
+ */
+function process_cellphone() {
+	run_smart_marketing_addon_sms_order_action( __FUNCTION__ );
 }
 
 /**
- * Add new interval to wordpress cron schedules
- * @param $schedules
+ * Add new interval to WordPress cron schedules
+ *
+ * @param array $schedules schedules array.
  *
  * @return mixed
  */
-function smsonw_my_add_every_fifteen_minutes($schedules) {
-    $schedules['every_fifteen_minutes'] = array(
-        'interval' => 60 * 15,
-        'display' => __('Every Fifteen Minutes')
-    );
-    return $schedules;
+function smsonw_my_add_every_fifteen_minutes( $schedules ) {
+	$schedules['every_fifteen_minutes'] = array(
+		'interval' => 60 * 15,
+		'display'  => __( 'Every Fifteen Minutes' ),
+	);
+	return $schedules;
 }
-add_filter('cron_schedules', 'smsonw_my_add_every_fifteen_minutes');
-// Schedule an action if it's not already scheduled
+add_filter( 'cron_schedules', 'smsonw_my_add_every_fifteen_minutes' );
+// Schedule an action if it's not already scheduled.
 if ( ! wp_next_scheduled( 'smsonw_my_add_every_fifteen_minutes' ) ) {
-    wp_schedule_event( time(), 'every_fifteen_minutes', 'smsonw_my_add_every_fifteen_minutes' );
+	wp_schedule_event( time(), 'every_fifteen_minutes', 'smsonw_my_add_every_fifteen_minutes' );
 }
 
-add_filter( 'upgrader_pre_install', 'filter_upgrader_pre_install', 10, 2 ); 
+add_filter( 'upgrader_pre_install', 'filter_upgrader_pre_install', 10, 2 );
 
-function filter_upgrader_pre_install($response, $hook_extra) {
+/**
+ * Pre intall filter upgrade
+ *
+ * @param string $response response.
+ * @param array  $hook_extra hook array.
+ */
+function filter_upgrader_pre_install( $response, $hook_extra ) {
 
-    $path = 'sms-orders-alertnotifications-for-woocommerce/smart-marketing-addon-sms-order.php';
-    
-    if ($hook_extra['plugin'] == $path){
-        if(version_compare(PLUGIN_NAME_VERSION, '1.5.2', '<'))
-            update_option('egoi_sms_counter', 0);
-    }
+	$path = 'sms-orders-alertnotifications-for-woocommerce/smart-marketing-addon-sms-order.php';
+
+	if ( $hook_extra['plugin'] === $path ) {
+		if ( version_compare( PLUGIN_NAME_VERSION, '1.5.2', '<' ) ) {
+			update_option( 'egoi_sms_counter', 0 );
+		}
+	}
 }
 
 
